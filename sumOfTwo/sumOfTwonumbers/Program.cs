ffddb9690c2b813bca46e07c8187eba973873fdb
“[Giorgian <[vatragiorgian@gmail.com]>
2025-07-22 23:48:19 +0300

    first C# project

---
+++ sumOfTwo/sumOfTwonumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Exercise2
+{
+    public static void Main()
+    {
+        int sum = SumOfTwoNumbers(2, 3);
+        Console.WriteLine("Sum: " + sum);
+
+    }
+     public static int SumOfTwoNumbers(int a, int b)
+        {
+
+            return a + b;
+
+        }
+}
+
+

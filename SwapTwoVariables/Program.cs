64f8bf4d78d47026eb7ee3823e34e4d806f18285
“[Giorgian <[vatragiorgian@gmail.com]>
2025-07-23 00:24:40 +0300

    Swaping two numbers problem done

---
+++ SwapTwoVariables/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+public class SwapTwoVariables
+{
+    static public void Main(string[] args)
+    {
+        int FirstValue, SecondValue, temp;
+        Console.Write("Input First Variable: ");
+        FirstValue = int.Parse(Console.ReadLine());
+        Console.Write("Input Second Variable: ");
+        SecondValue = int.Parse(Console.ReadLine());
+
+
+        Console.WriteLine("Before");
+        Console.WriteLine("First Value : " + FirstValue);
+        Console.WriteLine("Second Value : " + SecondValue);
+
+        temp = FirstValue;
+        FirstValue = SecondValue;
+        SecondValue = temp;
+
+
+        Console.WriteLine("After");
+        Console.WriteLine("First Value : " + FirstValue);
+        Console.WriteLine("Second Value : " + SecondValue);
+
+    }
+}

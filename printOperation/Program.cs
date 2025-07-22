b1816db08af5789aedf2b4a8a212df5bcba3d395
“[Giorgian <[vatragiorgian@gmail.com]>
2025-07-23 00:06:55 +0300

    Printe Operation project done

---
+++ printOperation/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+public class PrintOperation
+{
+    public static void Main()
+    {
+        Console.WriteLine(-1 + 4 * 6);
+        Console.WriteLine((35 + 5) % 7);
+        Console.WriteLine(14 + -4 * 6 / 11);
+        Console.WriteLine(2 + 15 / 6 * 1 - 7 % 2); 
+    }
+}

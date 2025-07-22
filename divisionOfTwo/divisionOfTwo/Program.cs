f7b7d4a6363479fc2e56d2a3dcdb2284a33c37d6
“[Giorgian <[vatragiorgian@gmail.com]>
2025-07-22 23:56:25 +0300

    division function problem resoution

---
+++ divisionOfTwo/divisionOfTwo/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+class Exercise3
+{
+    static public void Main()
+    {
+        Console.WriteLine(divisionOfTwo(15, 9)); 
+    }
+
+    static public int divisionOfTwo(int a, int b)
+    {
+        return a - b; 
+    }
+}

30bb4fd7809484fd588b51ccf3e1b1192c60e119
“[Giorgian <[vatragiorgian@gmail.com]>
2025-07-23 00:49:50 +0300

    pultiply three numbers done

---
+++ MultiplyThreeNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+static class MultiplyThreeNumbers
+{
+    static public void Main()
+    {
+
+        int FirstNumber, SecondNumber, ThirdNumber;
+
+        Console.Write("Input the First Number: ");
+        FirstNumber = int.Parse(Console.ReadLine());
+
+
+        Console.Write("Input the Second Number: ");
+        SecondNumber = int.Parse(Console.ReadLine());
+
+
+        Console.Write("Input the Third Number: ");
+        ThirdNumber = int.Parse(Console.ReadLine());
+
+
+        Console.WriteLine("First Number: " + FirstNumber);
+        Console.WriteLine("Second Number: " + SecondNumber);
+        Console.WriteLine("Third Number: " + ThirdNumber);
+
+        int Multiplication = FirstNumber * SecondNumber * ThirdNumber;
+        Console.WriteLine("Total : {0} + {1} + {2} = {3}", FirstNumber, SecondNumber, ThirdNumber, Multiplication); 
+
+
+    }
+}

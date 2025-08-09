d906f5c3ea3c386635524f58cd7556b860da2867
“[Giorgian <[vatragiorgian@gmail.com]>
2025-08-09 18:17:04 +0300

    average calculator for integers done

---
+++ AverageCalculator/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more informatio
+
+
+using System.ComponentModel.Design.Serialization;
+
+static class AverageCalculator
+{
+    public static void Main()
+    {
+
+        Console.WriteLine("Insert First Number: ");
+        int FirstNumber = int.Parse(Console.ReadLine());
+        Console.WriteLine("Insert Second Number: ");
+        int SecondNumber = int.Parse(Console.ReadLine());
+        Console.WriteLine("Insert Third Number: ");
+        int ThirdNumber = int.Parse(Console.ReadLine());
+        Console.WriteLine("Insert Forth Number: ");
+        int ForthNumber = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Average: {0}", (FirstNumber + SecondNumber + ThirdNumber + ForthNumber)/4);
+
+    }
+}

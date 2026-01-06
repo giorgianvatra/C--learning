cc8ae42808324d2e6a291864b63d2f2f2483a527
“[Giorgian <[vatragiorgian@gmail.com]>
2026-01-06 23:27:31 +0200

    division onf two numbers

---
+++ 3_divideTwoNumbers/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+public class divideTwoNumbers
+{
+    public static void Main()
+    {
+        int num1 = 5; 
+        int num2 = 7; 
+        long divide  = num2/num1; 
+        Console.WriteLine("Number One: " + num1);
+        Console.WriteLine("Number Two: " + num2);
+        Console.WriteLine("Division: " + divide);
+    }
+}

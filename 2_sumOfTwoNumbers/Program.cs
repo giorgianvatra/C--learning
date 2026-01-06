6555fb996df48980585e7042d9b272abd8e524fb
“[Giorgian <[vatragiorgian@gmail.com]>
2026-01-06 23:14:50 +0200

    sum of two

---
+++ 2_sumOfTwoNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System.Collections.Specialized;
+using System.Globalization;
+
+public class sumOfTwoNumbers
+{
+    public static void Main()
+    {
+        int num1  = 5; 
+        int num2 = 19; 
+        int sum = num1 + num2;
+        
+        Console.WriteLine("Number 1: " + num1 );    
+        Console.WriteLine("Number 2: " + num2 );    
+        Console.WriteLine("Sun: " + sum );    
+        return;     
+    }
+        
+    }

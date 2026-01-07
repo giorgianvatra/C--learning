d2886d93d6b9dc97db8173e64a581b4c36eb1677
“[Giorgian <[vatragiorgian@gmail.com]>
2026-01-07 21:11:46 +0200

    Multiply three numbers

---
+++ 4_Multiply_Three_Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+using System.Globalization;
+
+public class MultiplyThreeNumbers
+{
+    public static void Main()
+    {
+        Console.WriteLine("Input Fist Number");
+        int firstNumber = Convert.ToInt32(Console.ReadLine()); 
+        Console.WriteLine("Input Second Number"); 
+        int secondNumber = Convert.ToInt32(Console.ReadLine()); 
+        Console.WriteLine("Input Third Number"); 
+        int thirdNumber = Convert.ToInt32(Console.ReadLine()); 
+
+        Console.WriteLine("---------------");
+        int result = firstNumber * secondNumber * thirdNumber; 
+
+        Console.WriteLine("Result = " + result); 
+
+
+
+    }
+}

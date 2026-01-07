f79edbc29d5cc06d3da7ea2949c9cfbcba757bcd
“[Giorgian <[vatragiorgian@gmail.com]>
2026-01-07 21:27:53 +0200

    Arithmetic Operations problem Done

---
+++ 5_Arithmetic_Operations/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+using System.Diagnostics.CodeAnalysis;
+
+public class ArithmeticsOperations
+{
+    public static void Main()
+    {
+        Console.WriteLine("INPUT NUMBERS");
+        Console.WriteLine("Input First Number");
+        int firstNumber = Convert.ToInt32(Console.ReadLine());     
+        Console.WriteLine("Input Second Number");
+        int secondNumber = Convert.ToInt32(Console.ReadLine());     
+
+        Console.WriteLine( "{0} + {1} = {2}", firstNumber, secondNumber, Sum(firstNumber, secondNumber)); 
+        Console.WriteLine( "{0} - {1} = {2}", firstNumber, secondNumber, firstNumber - secondNumber); 
+        Console.WriteLine( "{0} * {1} = {2}", firstNumber, secondNumber, firstNumber * secondNumber); 
+        Console.WriteLine( "{0} / {1} = {2}", firstNumber, secondNumber, firstNumber / secondNumber); 
+        Console.WriteLine( "{0} % {1} = {2}", firstNumber, secondNumber, firstNumber % secondNumber); 
+
+
+    }
+
+    public static int Sum(int a, int b)
+    {
+        return a + b; 
+    }
+
+
+}

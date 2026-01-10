5c5bac73882617e6d3c5e10c62d3fc1ab0fe0f26
“[Giorgian <[vatragiorgian@gmail.com]>
2026-01-10 18:30:41 +0200

    Multiplication table problem done

---
+++ 6_Multiplication_Table/Program.cs
@@ -0,0 +1,16 @@
+﻿public class MultiplicationTable
+{
+    public static void Main()
+    {
+        Console.WriteLine("INSERT THE NUMBER");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        for(int i  = 1; i <= 10; i++)
+        {
+            Console.WriteLine("{0} * {1} = {2}", i, number, i * number); 
+        }
+
+    
+    }
+    
+}

1d422a01aa91cf303ce29fab86c99ddfbe40fc4d
“[Giorgian <[vatragiorgian@gmail.com]>
2026-01-10 18:42:24 +0200

    Average of Four numbers problem done

---
+++ 7_Average_Of_Four_Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+using System.Linq.Expressions;
+
+public class AverageOfFourNumbers
+{
+    public static void Main()
+    {
+        Console.WriteLine("Welcome to the 'Average of Four program"); 
+        Console.WriteLine("Insert First Number: "); 
+        double firstNumber = Convert.ToInt32(Console.ReadLine()); 
+        Console.WriteLine("Insert Second Number: "); 
+        double secondNumber = Convert.ToInt32(Console.ReadLine()); 
+        Console.WriteLine("Insert Third Number: "); 
+        double thirdNumber = Convert.ToInt32(Console.ReadLine()); 
+        Console.WriteLine("Insert Fourth Number: "); 
+        double fourthNumber = Convert.ToInt32(Console.ReadLine()); 
+
+        double average =  (firstNumber + secondNumber + thirdNumber + fourthNumber)/4; 
+
+        Console.WriteLine("The Average of the Four Numbers is: " + average); 
+    }
+}

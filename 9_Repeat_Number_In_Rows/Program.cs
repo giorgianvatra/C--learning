0a7dd95f5d94d26cc3fe22cef3982e354e2a6422
“[Giorgian <[vatragiorgian@gmail.com]>
2026-01-11 22:03:27 +0200

    Repeat number in rows proble done

---
+++ 9_Repeat_Number_In_Rows/Program.cs
@@ -0,0 +1,23 @@
+﻿
+public class PrepeatNumberInRows
+{
+    public static void Main()
+    {
+        Console.WriteLine("Welcome to Reprea number in Rows Program!"); 
+        Console.WriteLine("Please input a number in order to perform the program!");
+        long number = Convert.ToInt64(Console.ReadLine());  
+
+        for(int i = 1; i <= 4; i++)
+        {
+            if(i%2 == 1)
+            {
+                Console.WriteLine("{0} {0} {0} {0}", number); 
+            }
+            if(i%2 == 0)
+            {
+                Console.WriteLine("{0}{0}{0}{0}", number); 
+            }
+        }
+
+    }
+}

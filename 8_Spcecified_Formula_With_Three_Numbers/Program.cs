a601ec686220224bc0592290f2ece438050a595e
“[Giorgian <[vatragiorgian@gmail.com]>
2026-01-11 21:35:01 +0200

    Specified Formula with Three Numbers

---
+++ 8_Spcecified_Formula_With_Three_Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿
+public class SpecifiedFormulaWithThreeNumbers
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello");
+        Console.WriteLine("Please input three numbers the program will perfor (x+y)*z and x*y + y*z");
+        Console.WriteLine("Input First Number"); 
+        double firstNumber = Convert.ToInt64(Console.ReadLine());
+        Console.WriteLine("Input Second Number"); 
+        double secondNumber = Convert.ToInt64(Console.ReadLine());
+        Console.WriteLine("Input Third Number"); 
+        double thirdNumber = Convert.ToInt64(Console.ReadLine());
+
+        Console.WriteLine("({0} + {1}) * {2} = {3}", firstNumber, secondNumber, thirdNumber, (firstNumber + secondNumber) * thirdNumber); 
+        Console.WriteLine("{0}*{1} + {1}*{2} = {3}", firstNumber, secondNumber, thirdNumber, (firstNumber * secondNumber) + (secondNumber * thirdNumber)); 
+
+    }
+}

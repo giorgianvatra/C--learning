302ef36e7b005512b26d80479c52ecf04a856b04
“[Giorgian <[vatragiorgian@gmail.com]>
2026-01-19 23:31:52 +0200

    Rectangle pattern with numbers problem done

---
+++ 10_Rectangle_pattern_with_Number/Program.cs
@@ -0,0 +1,46 @@
+﻿
+
+using System.Formats.Asn1;
+
+public class RectanglePatternWithNumber
+{
+    public static void Main()
+    {
+        Console.WriteLine("Welcome the the Rectangle Pattern With Numbers program");
+        Console.WriteLine("Please insert the number to begin the program!");
+        long  number = Convert.ToInt64(Console.ReadLine()); 
+        for(int i = 0; i < 5; i++)
+        {
+            if(i == 0 || i == 4)
+            {
+                string row = ""; 
+                for(int j = 0; j < 3; j++)
+                {
+                 row += number + " "; 
+                }
+                Console.WriteLine(row);
+            }
+            else
+            {
+                string row = ""; 
+                for(int j = 0; j < 3; j++)
+                {
+                    if(j == 0 || j == 2)
+                    {
+                        row +=  number + " ";
+                    }
+                    else
+                    {
+                        row += "  ";
+                    }
+                 
+                }
+                Console.WriteLine(row);
+            }
+
+
+        }
+        
+    }
+    
+}
